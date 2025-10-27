947a7fd70cd8736b29f8161edcc23ecec2101572
eckofox1981 <eckofox1981@pm.me>
2025-10-27 21:54:16 +0100

    build: application (first commit)

---
+++ src/main/java/ecko/fox/foxy_eckoes/FoxyEckoesApplication.java
@@ -0,0 +1,13 @@
+package ecko.fox.foxy_eckoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxyEckoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxyEckoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecko/fox/foxy_eckoes/FoxyEckoesApplicationTests.java
@@ -0,0 +1,13 @@
+package ecko.fox.foxy_eckoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxyEckoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
