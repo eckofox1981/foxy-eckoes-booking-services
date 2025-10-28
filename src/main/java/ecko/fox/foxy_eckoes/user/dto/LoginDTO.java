f6ca2e6b864733b34a7c48b2333a99d942ece513
eckofox1981 <eckofox1981@pm.me>
2025-10-28 14:40:33 +0100

    fix: remove superflous passwordConfirm variable

--- src/main/java/ecko/fox/foxy_eckoes/user/dto/LoginDTO.java
+++ src/main/java/ecko/fox/foxy_eckoes/user/dto/LoginDTO.java
@@ -1,12 +1,13 @@
 package ecko.fox.foxy_eckoes.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class LoginDTO {
     private String username;
     private String password;
-    private String passwordConfirm;
 }
