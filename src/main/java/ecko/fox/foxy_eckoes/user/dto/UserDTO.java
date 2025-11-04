19f48f0d3f87ac5722a4b03b17f028e59ff788e1
eckofox1981 <eckofox1981@pm.me>
2025-11-04 14:55:33 +0100

    feat: add sorting og user's booking before returning DTO

--- src/main/java/ecko/fox/foxy_eckoes/user/dto/UserDTO.java
+++ src/main/java/ecko/fox/foxy_eckoes/user/dto/UserDTO.java
@@ -1,10 +1,13 @@
 package ecko.fox.foxy_eckoes.user.dto;
 
+import ecko.fox.foxy_eckoes.booking.Booking;
 import ecko.fox.foxy_eckoes.booking.BookingDTO;
+import ecko.fox.foxy_eckoes.event.Event;
 import ecko.fox.foxy_eckoes.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,6 +33,7 @@ public class UserDTO {
                 user.getBookings()
                         .stream()
                         .map((BookingDTO::fromBooking))
+                        .sorted(Comparator.comparing(booking -> booking.getEvent().getDate()))
                         .toList(),
                 user.getRole(),
                 user.getOpenIdProvider()
