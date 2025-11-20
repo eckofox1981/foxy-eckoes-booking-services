7c9a13d3cea7a4461eee8adea947af04a02e8c09
eckofox1981 <eckofox1981@pm.me>
2025-11-20 10:13:56 +0100

    fix: remove openId related function (unused)

--- src/main/java/ecko/fox/foxy_eckoes/user/UserRepository.java
+++ src/main/java/ecko/fox/foxy_eckoes/user/UserRepository.java
@@ -9,6 +9,4 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
-
-    Optional<User> findByOpenId(String openId);
 }
